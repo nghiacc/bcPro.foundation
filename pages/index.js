2b4efe6f1f0587d2464ac8dc289ed1ae1d4903c1
nghiacc <nghiacc@gmail.com>
2022-01-19 16:43:49 +0700

    add token text and adjust icon size

--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,14 @@ export default function Home() {
         
         <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSTCLb9cjqnl_V0u71h2XFWDSG75aQ9sWg_QXhhp0BZDi3AhTK12wF2lX10BITGFfzxqQyRFgvLdeXp/embed?start=true&loop=true&delayms=5000" frameborder="0" width="100%" height="575" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
       
-        <table>
+        <h3 className='description'>Token Informations</h3>
+        <table width={200}>
           <tr>
             <td>
-              <a target="_blank" title="bcPro Token" rel="noopener noreferrer"  href="https://explorer.be.cash/tx/5a68fe2909240832f5e3de0d57cb8d622934f47ee7ab7b2ba49d5295a581ca13"><img src="/bcpro-icon.png" width={32} height={32}></img></a>
+              <a target="_blank" title="bcPro Token" rel="noopener noreferrer"  href="https://explorer.be.cash/tx/5a68fe2909240832f5e3de0d57cb8d622934f47ee7ab7b2ba49d5295a581ca13"><img src="/bcpro-icon.png" width={64} height={64}></img></a>
             </td>
             <td>
-              <a target="_blank" title="EAT Token" rel="noopener noreferrer"  href="https://explorer.be.cash/tx/0c8d48bb808cf84705cd4c7cf27807d439a5dd032f662116966263226424805a"><img src="/eat-icon.png" width={32} height={32}></img></a>
+              <a target="_blank" title="EAT Token" rel="noopener noreferrer"  href="https://explorer.be.cash/tx/0c8d48bb808cf84705cd4c7cf27807d439a5dd032f662116966263226424805a"><img src="/eat-icon.png" width={64} height={64}></img></a>
             </td>
           </tr>
         </table>
