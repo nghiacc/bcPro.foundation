3cd72c640195f22290da6193f0b0c50d4e37b943
nghiacc <nghiacc@gmail.com>
2022-06-06 16:54:20 +0700

    removed embedded slide

--- pages/index.js
+++ pages/index.js
@@ -78,10 +78,10 @@ export default function Home() {
         </table>
         <p>&nbsp;
         </p>  
-        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSTCLb9cjqnl_V0u71h2XFWDSG75aQ9sWg_QXhhp0BZDi3AhTK12wF2lX10BITGFfzxqQyRFgvLdeXp/embed?start=true&loop=true&delayms=5000" frameborder="0" width="100%" height="575" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+        
         
         <p>
-          More info: Check out <a target="_blank" title="bcPro Token" rel="noopener noreferrer" href='https://t.me/bcprotx'>bcPro Telegram channel</a>
+          More info: Check out <a target="_blank" title="bcPro Token" rel="noopener noreferrer" href='https://docs.google.com/presentation/d/e/2PACX-1vSTCLb9cjqnl_V0u71h2XFWDSG75aQ9sWg_QXhhp0BZDi3AhTK12wF2lX10BITGFfzxqQyRFgvLdeXp'>bcPro Tokenomics</a>
         </p>
       </main>
 
